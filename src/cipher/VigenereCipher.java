12ff85802a637119e7f9eb63eeb4507c31de1573
sderevianko <derevianko.serhii@gmail.com>
2020-06-07 17:04:49 +0300

    Corrected small issue

--- src/cipher/VigenereCipher.java
+++ src/cipher/VigenereCipher.java
@@ -4,22 +4,18 @@ public class VigenereCipher implements CipherMethod<String> {
     @Override
     public String doEncryption(String message, String key) {
         final StringBuilder encrypted = new StringBuilder();
-        final char startingCharacter = 'a';
+        final char startingCharacter = 'A';
         final int alphabetPower = 26;
         final int numberMessage = message.length();
         final int numberKey = key.length();
         final char[] messageArray = message.toCharArray();
         final char[] keyArray = key.toCharArray();
         int keyIndex = 0;
-        for (int messageIndex = 0; messageIndex < numberMessage; messageIndex++) {
-            int newAlphabetPosition =  startingCharacter + (messageArray[messageIndex] + keyArray[keyIndex]) % alphabetPower;
+        for (int messageIndex = 0; messageIndex < numberMessage; ++messageIndex) {
+            int newAlphabetPosition = startingCharacter + ((messageArray[messageIndex] + keyArray[keyIndex]) % alphabetPower);
             char newCharacter = (char) (newAlphabetPosition);
             encrypted.append(newCharacter);
-            if (keyIndex == (numberKey-1)) {
-                keyIndex = 0;
-            } else {
-                keyIndex++;
-            }
+            keyIndex = ++keyIndex % numberKey;
         }
         return encrypted.toString();
     }
--- src/mainapplication/SimpleDataEncryptorApplication.java
+++ src/mainapplication/SimpleDataEncryptorApplication.java
@@ -10,11 +10,7 @@ public class SimpleDataEncryptorApplication {
 
     public static void main(String[] args) {
         // The main condition of Strategy Pattern: design pattern that enables selecting an algorithm at runtime
-        final int randomNumber = new Random().nextInt(2);
-        final BaseEncryptor encryptor = randomNumber != 0
-                ? new AesEncryptor()
-                : new VigenereEncryptor();
-        System.out.println(encryptor.encrypt("Hi, people!", "bithitgit"));
+        System.out.println(new VigenereEncryptor().encrypt("HELLOMYDEARINTERLOCUTOR", "ENCRYPTMYMESSAGEPLEASE"));
     }
 }
 
