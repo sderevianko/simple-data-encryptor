06ca908e0c0fac9840ad0b6d2da119a03abe8610
sderevianko <derevianko.serhii@gmail.com>
2020-06-10 00:04:40 +0300

    Changes in Vigenere cipher

--- src/cipher/VigenereCipher.java
+++ src/cipher/VigenereCipher.java
@@ -10,12 +10,21 @@ public class VigenereCipher implements CipherMethod<String> {
         final int numberKey = key.length();
         final char[] messageArray = message.toCharArray();
         final char[] keyArray = key.toCharArray();
+        int newAlphabetPosition;
+        char newCharacter;
         int keyIndex = 0;
+
         for (int messageIndex = 0; messageIndex < numberMessage; ++messageIndex) {
-            int newAlphabetPosition = startingCharacter + ((messageArray[messageIndex] + keyArray[keyIndex]) % alphabetPower);
-            char newCharacter = (char) (newAlphabetPosition);
-            encrypted.append(newCharacter);
-            keyIndex = ++keyIndex % numberKey;
+            if (((messageArray[messageIndex] <= 64) && (messageArray[messageIndex] >= 32)) || ((messageArray[messageIndex] <= 96) && (messageArray[messageIndex] >= 91)) || ((messageArray[messageIndex] <= 126) && (messageArray[messageIndex] >= 123))) {
+                newAlphabetPosition = messageArray[messageIndex];
+                newCharacter = (char) (newAlphabetPosition);
+                encrypted.append(newCharacter);
+            } else {
+                newAlphabetPosition = startingCharacter + ((messageArray[messageIndex] + keyArray[keyIndex]) % alphabetPower);
+                newCharacter = (char) (newAlphabetPosition);
+                encrypted.append(newCharacter);
+                keyIndex = ++keyIndex % numberKey;
+            }
         }
         return encrypted.toString();
     }
--- src/mainapplication/SimpleDataEncryptorApplication.java
+++ src/mainapplication/SimpleDataEncryptorApplication.java
@@ -10,7 +10,11 @@ public class SimpleDataEncryptorApplication {
 
     public static void main(String[] args) {
         // The main condition of Strategy Pattern: design pattern that enables selecting an algorithm at runtime
-        System.out.println(new VigenereEncryptor().encrypt("HELLOMYDEARINTERLOCUTOR", "ENCRYPTMYMESSAGEPLEASE"));
+        final int randomNumber = new Random().nextInt(2);
+        final BaseEncryptor encryptor = randomNumber != 0
+            ? new AesEncryptor()
+            : new VigenereEncryptor();
+        System.out.println(new VigenereEncryptor().encrypt("HELLO MY DEAR INTERLOCUTOR!", "ENCRYPTMYMESSAGEPLEASE"));
     }
 }
 
